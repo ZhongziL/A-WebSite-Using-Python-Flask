b5f63c8e043b9a8b127050c70a5b19a8b1bb4b71
ZhongziL <liangzzh@mail2.sysu.edu.cn>
2017-05-06 01:56:22 -0700

    a simple database (c)

--- manage.py
+++ manage.py
@@ -1,14 +1,19 @@
 from flask_script import Manager, Shell
-from app import create_app
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
 
 app = create_app('development')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 '''add command python manage.py shell'''
 def make_shell_context():
     return dict(app=app)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
+
+'''add command python manage.py db ..'''
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
