d8c1d1f4078ac12e05bbacfd358bc0255f0ecd23
ZhongziL <liangzzh@mail2.sysu.edu.cn>
2017-05-08 08:21:38 -0700

    add database migrations (e)

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
+from app.models import User
 
 app = create_app('development')
 manager = Manager(app)
@@ -8,7 +9,7 @@ migrate = Migrate(app, db)
 
 '''add command python manage.py shell'''
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, User=User)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
