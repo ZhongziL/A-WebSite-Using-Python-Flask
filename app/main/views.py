53b46e03cb3c16fd8eeee7a30b9430f9e0f411cb
ZhongziL <liangzzh@mail2.sysu.edu.cn>
2017-05-21 13:13:30 +0800

    add profile (i)

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,16 @@
 from . import main
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask_login import login_required
+from ..models import User
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/user/<name>')
+@login_required
+def user(name):
+    user = User.query.filter_by(username=name).first()
+    if user is None:
+        return redirect(url_for('auth.login'))
+    return render_template('profile.html', user=user)
